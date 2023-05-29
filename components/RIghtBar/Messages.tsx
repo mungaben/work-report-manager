2dcf7e660298c598683acc9d1650e3ca5bb39676
mungaben <kamaumungai3742@gmail.com>
2023-05-29 11:51:11 +0300

    updated right hard profile

--- components/RIghtBar/Messages.tsx
+++ components/RIghtBar/Messages.tsx
@@ -4,47 +4,56 @@
 
 import React from 'react'
 import Image from 'next/image';
-export const data=[
-      {
-        "id": 1,
-        "name": "John Doe",
-        "avatar": "https://avatars1.githubusercontent.com/u/105234?v=4",
-        "message": "Hello, how are you? I'm a developer and I'm a great developer"
-      },
-      {
-        "id": 2,
-        "name": "John Doe",
-        "avatar": "https://avatars1.githubusercontent.com/u/105234?v=4",
-        "message": "Hello, how are you? I'm a developer and I'm a great developer"
-      }
+export const data = [
+  {
+    "id": 1,
+    "name": "John Doe",
+    "avatar": "https://avatars1.githubusercontent.com/u/105234?v=4",
+    "message": " developer"
+  },
+  {
+    "id": 2,
+    "name": "John Doe",
+    "avatar": "https://avatars1.githubusercontent.com/u/105234?v=4",
+    "message": 'developer'
+  }
 
 
-    ]
-  
+]
+
 
 const Messages = () => {
   return (
-    <div className=' w-full flex flex-col  transition bg-green-400  '>
-        <div className='text-[#333333]/90 text-4xl mx-4 text-center  p-2 hover:underline font-semibold  '>Message</div>
-        <div className=' w-full  '>
-            {data.map((item)=>(
-                <div key={item.id} className='transition w-full flex-1 justify-center  '>
-                    <div className=' flex flex-row m-2  w-full'>
-                       <Image
-                       src={item.avatar}
-                       width={20}
-                       height={20}
-                       alt="avatar"
-                       className='rounded-full m-2 justify-center items-center  '
-                       />
-                       <p className='text-3xl text-center items-center  tracking-wider  '>{item.name}</p>
-                    </div>
-                    <div className=' text-center items-center p-2 m-2  '>
-                        <p className='text-xl text-center w-full break-words truncate'>{item.message}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
+    <div className=' w-full flex flex-col  transition  justify-start  '>
+      <div className='text-[#333333]/90 text-4xl mx-4 text-center  p-2 hover:underline font-semibold  '>Message</div>
+      <div className=' w-full  bg-green-500 flex justify-start flex-col '>
+        {data.map((item) => (
+          <div key={item.id} className='transition w-full flex-1 justify-start  bg-red-500 m-4 p-1   '>
+            <div className=' flex flex-row mx-2  w-full justify-start items-start mb-0 pb-0  '>
+              <Image
+                src={item.avatar}
+                width={50}
+                height={50}
+                alt="avatar"
+                className='rounded-full mx-2 mb-0 justify-center items-center  '
+              />
+              <div className=' flex w-full flex-col '>
+               <div className='w-full'>
+               <p className='text-3xl text-center items-center  tracking-wider  '>{item.name}</p>
+
+               </div>
+                
+
+                <p className='text-xl text-center items-start justify-start w-full break-words truncate'>{item.message}</p>
+
+
+              </div>
+
+            </div>
+
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- components/RIghtBar/RightBar.tsx
+++ components/RIghtBar/RightBar.tsx
@@ -4,12 +4,12 @@ import Messages from './Messages'
 
 const RightBar = () => {
   return (
-    <div className=' w-full  top-5 mt-5 flex   p-2'>
-      <div className=' flex flex-col items-center '>
-        <div className=' w-full flex justify-center items-center'>
+    <div className=' w-full  top-5 mt-5 flex justify-center relative   '>
+      <div className=' flex flex-col  justify-center top-0 '>
+        <div className=' w-full flex justify-center items-center  '>
           <Profile />
         </div>
-        <div className='flex w-full'>
+        <div className='bg-yellow-500 w-full absolute '>
           <Messages />
         </div>
 
