39356dbdde17b53d52fc9c73f6b336d8ce4a4152
mungaben <kamaumungai3742@gmail.com>
2023-06-30 15:28:55 +0300

    next auth change

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import * as bcrypt from "bcryptjs";
 
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
 
 
@@ -16,54 +16,6 @@ interface User {
   email?: string | null | undefined;
   image?: string | null | undefined;
 }
-
-// export const authOptions: NextAuthOptions = {
-//   session: {
-//     strategy: "jwt",
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       name: "Sign in",
-//       credentials: {
-//         email: {
-//           label: "Email",
-//           type: "email",
-//           placeholder: "example@example.com",
-//         },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         if (!credentials?.email || !credentials.password) {
-//           return null;
-//         }
-
-//         const user = await prisma.user.findUnique({
-//           where: {
-//             email: credentials.email,
-//           },
-//         });
-
-//         if (!user || !(await compare(credentials.password, user.password))) {
-//           return null;
-//         }
-
-//         return {
-//           id: user.id,
-//           email: user.email,
-//           name: user.name,
-//           randomKey: "Hey cool",
-//         };
-//       },
-//     }),
-//   ],
-// };
-
-
-
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST }
-
-
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -88,32 +40,6 @@ export const authOptions: NextAuthOptions = {
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
 
-        // const res = await fetch("http://localhost:3000/api/Login", {
-        //   method: "POST",
-        //   body: JSON.stringify({
-        //     name: credentials?.username,
-        //     email: credentials?.email,
-        //     password: credentials?.password
-        //   }),
-        //   headers: {
-        //     "Content-Type": "application/json"
-        //   }
-        // })
-        // // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-        // const user = await res.json()
-
-
-        // if (user) {
-        //   // Any object returned will be saved in `user` property of the JWT
-        //   console.log("user",user);
-
-        //   return user
-        // } else {
-        //   // If you return null then an error will be displayed advising the user to check their details.
-        //   return null
-
-        //   // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        // }
 
         if (!credentials?.email || !credentials.password) {
           // console.log("no user credentials");
