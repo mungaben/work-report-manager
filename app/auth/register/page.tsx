e0b243bdee55883bdc22c333e46d4d2b5cfeeb8b
mungaben <kamaumungai3742@gmail.com>
2023-06-30 11:43:23 +0300

    comm

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,133 +1,148 @@
-'use client'
+"use client";
 
-import { useState } from "react"
-import axios from "axios"
-import { toast } from "react-hot-toast"
-import { useRouter } from "next/navigation"
-import Link from "next/link"
-import Image from "next/image"
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Register() {
-  const [RegisterUser, setRegisterUser] = useState(false)
-    const router = useRouter()
-    const [data, setData] = useState({
-         name: '',
-          email: '',
-           password: ''
-         })
+  const [RegisterUser, setRegisterUser] = useState(false);
+  const router = useRouter();
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-         const registerUser = async (e:React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault()
-            setRegisterUser(true)
-            axios.post('/api/User', data)
-            .then((datas) => {
-              
-                
-                toast.success('User has been registered!')
-                // router.push('/Main')
-              }
-                )
+  const registerUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setRegisterUser(true);
+    axios
+      .post("/api/User", data)
+      .then((datas) => {
+        toast.success("User has been registered!");
+        // router.push('/Main')
+      })
 
-            .catch((errors) => toast.error(errors))
-         }
+      .catch((errors) => toast.error(errors));
+  };
 
-    return (
-      <>
-        <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <Image
-              className="w-auto h-10 mx-auto"
-              src={'/Nairobiwater.png'}
-              alt="Nairobi water"
-              width={100}
-              height={100}
-            />
-            <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">
-              Register for an account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={registerUser}>
-              <div>
-                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
-                  Name
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    required
-                    value={data.name}
-                    onChange={e => setData({ ...data, name: e.target.value })}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    value={data.email}
-                    onChange={e => setData({ ...data, email: e.target.value })}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    value={data.password}
-                    onChange={e => setData({ ...data, password: e.target.value })}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  disabled={RegisterUser}
-                  // onClick={()=>setRegisterUser(true)}
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Register
-                </button>
-              </div>
-            </form>
-  
-            <p className="mt-10 text-sm text-center text-gray-500">
-              Have an Account{' '}
-              <Link href={"auth/signin"} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                sign In
-              </Link>
-            </p>
-          </div>
+  return (
+    <>
+      <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Image
+            className="w-auto h-10 mx-auto"
+            src={"/Nairobiwater.png"}
+            alt="Nairobi water"
+            width={100}
+            height={100}
+          />
+          <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">
+            Register for an account
+          </h2>
         </div>
-      </>
-    )
-  }
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={registerUser}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  value={data.name}
+                  onChange={(e) => setData({ ...data, name: e.target.value })}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  value={data.email}
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={data.password}
+                  onChange={(e) =>
+                    setData({ ...data, password: e.target.value })
+                  }
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                disabled={RegisterUser}
+                // onClick={()=>setRegisterUser(true)}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-sm text-center text-gray-500">
+            Have an Account{" "}
+            <Link
+              href={"auth/signin"}
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              sign In
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
