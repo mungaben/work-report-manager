ba62da99a306a470b56c745a04dd06596275f014
mungaben <kamaumungai3742@gmail.com>
2023-06-30 10:23:53 +0300

    calender data change

--- components/Dashoboard/GetDataTime/Calender.tsx
+++ components/Dashoboard/GetDataTime/Calender.tsx
@@ -1,62 +1,61 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Calendar from 'react-calendar';
+import Calendar from "react-calendar";
 
-import 'react-calendar/dist/Calendar.css';
+import "react-calendar/dist/Calendar.css";
 import dayjs, { Dayjs } from "dayjs";
-import 'dayjs/locale/en'; // Import the desired locale file (e.g., English)
-import weekday from 'dayjs/plugin/weekday';
-import { useDatetime } from '../../../app/utils/Stores/Datetime';
-
+import "dayjs/locale/en"; // Import the desired locale file (e.g., English)
+import weekday from "dayjs/plugin/weekday";
+import { useDatetime } from "../../../app/utils/Stores/Datetime";
 
 dayjs.extend(weekday); // Extend dayjs with the weekday plugin
 
 type CalenderProps = {
-    settoggle:(toggle:boolean)=>void
-    toggle:boolean
-    setDay:(day:Date)=>void
-    day:Date
+  settoggle: (toggle: boolean) => void;
+  toggle: boolean;
+  setDay: (day: Date) => void;
+  day: Date;
+};
+const Calender: React.FC<CalenderProps> = ({ settoggle, toggle }) => {
+  const { date, setDate } = useDatetime();
+  const [ActualDtae, setActualDtae] = useState(new Date());
 
+  const [selectedDate, setSelectedDate] = useState(date);
 
+  console.log("selectedDate", selectedDate);
 
-}
-const Calender:React.FC<CalenderProps> = ({settoggle,toggle} ) => {
-    const {date,setDate }=useDatetime();
+  const handleDateChange = (
+    value: Date | Date[],
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    const date = Array.isArray(value) ? value[0] : value;
+    console.log("date", date);
 
+    setDate(date);
+    setSelectedDate(date);
 
+    settoggle(!toggle);
+  };
 
-    const [selectedDate, setSelectedDate] = useState(date);
-   
-    console.log("selectedDate", selectedDate);
-   
-    
+  const TodayDate = dayjs().endOf("day").toDate();
 
-    const handleDateChange = (date: Date | Date[], event: React.MouseEvent<HTMLButtonElement>) => {
-       event.preventDefault();
-        if (Array.isArray(date)) {
-            // Handle multiple dates if needed
-            return;
-        }
-        console.log("date", date);
+  return (
+    <div className=" transition animate-bounce delay-[2000] hover:animate-none  hover:delay-500">
+      <div className="flex flex-col ">
+        <h1 className=" flex justify-center items-center text-[#333333]/60 p-2">
+          Selected Date: {selectedDate.toLocaleDateString()}
+        </h1>
+        <Calendar
+          onChange={() => {
+            handleDateChange;
+          }}
+          value={selectedDate}
+          maxDate={TodayDate}
+        />
+      </div>
+    </div>
+  );
+};
 
-        setDate(date);
-        setSelectedDate(date);
-       
-        settoggle(!toggle);
-    };
-    const TodayDate = dayjs().endOf('day').toDate();
-
-    return (
-        <div className=' transition animate-bounce delay-[2000] hover:animate-none  hover:delay-500' >
-            <div className='flex flex-col '>
-                <h1 className=' flex justify-center items-center text-[#333333]/60 p-2' >Selected Date: {selectedDate.toLocaleDateString()}</h1>
-                <Calendar onChange={handleDateChange}
-                    value={selectedDate}
-                    maxDate={TodayDate}
-                />
-            </div>
-        </div>
-    )
-}
-
-export default Calender
+export default Calender;
